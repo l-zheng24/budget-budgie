87ff9e1e76aa269ad456a17a18dc53d3e02140de
mesierzega <me.sierzega@gmail.com>
2023-04-02 01:02:16 -0400

    changed design

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,91 +1,79 @@
-import React, { useState } from 'react'
-import { Progress } from "@material-tailwind/react"
+import React, { useState } from "react";
+import { Progress } from "@material-tailwind/react";
 
 function Dashboard() {
-  
-    return (
-      <React.Fragment>
+  return (
+    <React.Fragment>
       <section class="bg-slate-100">
-        
-        
-        <div class='w-screen h-screen grid text-white text-4xl md:grid-cols-2'>
-              
-              
-              <div class="flex justify-center pt-20 ">
-
-                
-                
-                
-                <div
-                  class=" flex-col rounded-lg  shadow-lg bg-light-blue w-5/6 h-4/5 flex">
-                  <div class=" flex-col justify-start p-6 flex">
-                    <h5
-                      class="mb-2 text-4xl font-medium text-dark-green  bg-amber-200 p-3 rounded-md">
-                      Dining Dollars
-                    </h5>
-                    <p class="mb-4 text-3xl  text-dark-green pt-3">
-                      Dining Dollars spent this month:
-                    </p>
-                    <div class = "w-full text-2xl bg-slate-100 text-dark-green rounded-md  p-2  h-1/4 inline">
-                      test
-                    </div>
-                    <p class="mb-4 text-3xl  text-dark-green">
-                      Dining Dollars spent this semester:
-                    </p>
-                    <div class = "w-full text-2xl bg-slate-100 text-dark-green rounded-md  p-2  h-1/4 inline">
-                      test
-                    </div>
-                    <p class="mb-4 text-3xl  text-dark-green">
-                      Dining Dollars spent this week:
-                    </p>
-                    <div class = "w-full text-2xl bg-slate-100 text-dark-green rounded-md  p-2  h-1/4 inline">
-                      test
-                    </div>
-                  </div>
+        <h class="pt-5 flex font-normal text-6xl justify-center text-dark-green">
+          Summary of Expenses
+        </h>
+        <div class="w-screen h-screen grid text-white text-4xl md:grid-cols-2">
+          <div class="flex justify-center pt-10 ">
+            <div class=" flex-col rounded-lg  shadow-lg bg-light-blue w-5/6 h-4/5 flex">
+              <div class=" flex-col justify-start p-4 flex">
+                <h5 class="mb-2 text-4xl font-medium text-dark-green  bg-amber-200 p-3 rounded-md">
+                  Dining Dollars
+                </h5>
+                <p class="mb-4 text-3xl  text-dark-green pt-3">
+                  Dining Dollars spent this month:
+                </p>
+                <div class="w-full text-2xl bg-slate-100 text-dark-green rounded-md  p-2  h-1/4 inline">
+                  test
                 </div>
-
-              </div>  
-            
-            
-          
-            <div class="flex justify-center pt-20 ">
-              <div
-                class=" flex-col rounded-lg shadow-lg bg-light-blue w-5/6 h-4/5 flex ">
-                <div class="flex flex-col justify-start p-6">
-                  <h5
-                    class="mb-2 text-4xl font-medium text-dark-green  bg-amber-200 p-3 rounded-md">
-                    Real Money
-                  </h5>
-                  <p class="mb-4 text-3xl text-dark-green pt-3">
-                      Amount spent this month:
-                    </p>
-                    <div class = "w-full text-2xl bg-slate-100 text-dark-green rounded-md  p-2  h-1/4 inline">
-                      test
-                    </div>
-                    <p class="mb-4 text-3xl text-dark-green">
-                      Amount spent this semester:
-                    </p>
-                    <div class = "w-full text-2xl bg-slate-100 text-dark-green rounded-md  p-2  h-1/4 inline">
-                      test
-                    </div>
-                    <p class="mb-4 text-3xl text-dark-green">
-                      Amount spent this week:
-                    </p>
-                    <div class = "w-full text-2xl bg-slate-100 text-dark-green rounded-md  p-2  h-1/4 inline">
-                      test
-                    </div>
+                <p class="my-4 text-3xl  text-dark-green">
+                  Dining Dollars spent this semester:
+                </p>
+                <div class="w-full text-2xl bg-slate-100 text-dark-green rounded-md  p-2  h-1/4 inline">
+                  test
+                </div>
+                <p class=" my-4 text-3xl  text-dark-green">
+                  Dining Dollars spent this week:
+                </p>
+                <div class="w-full text-2xl bg-slate-100 text-dark-green rounded-md  p-2  h-1/4 inline">
+                  test
                 </div>
               </div>
-            </div>  
-            
+            </div>
+          </div>
 
+          <div class="flex justify-center pt-10 ">
+            <div class=" flex-col rounded-lg shadow-lg bg-light-blue w-5/6 h-4/5 flex ">
+              <div class="flex flex-col justify-start p-6">
+                <h5 class="mb-2 text-4xl font-medium text-dark-green  bg-amber-200 p-3 rounded-md">
+                  Personal Money
+                </h5>
+                <p class="mb-4 text-3xl text-dark-green pt-3">
+                  Amount spent this month:
+                </p>
+                <div class="w-full text-2xl bg-slate-100 text-dark-green rounded-md  p-2  h-1/4 inline">
+                  test
+                </div>
+                <p class="my-4 text-3xl text-dark-green">
+                  Amount spent this semester:
+                </p>
+                <div class="w-full text-2xl bg-slate-100 text-dark-green rounded-md  p-2  h-1/4 inline">
+                  test
+                </div>
+                <p class="my-4 text-3xl text-dark-green">
+                  Amount spent this week:
+                </p>
+                <div class="w-full text-2xl bg-slate-100 text-dark-green rounded-md  p-2  h-1/4 inline">
+                  test
+                </div>
+                <div
+                  class="m-3 mb-4 rounded-lg bg-emerald-800 py-2 px-4 text-base text-danger-700 text-center"
+                  role="alert"
+                >
+                  Your current budget is: {localStorage.getItem("budget")}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </React.Fragment>
-    );
-  }
+  );
+}
 
-  export default Dashboard;
-
-
-  
+export default Dashboard;
--- src/components/RealMoney.js
+++ src/components/RealMoney.js
@@ -1,40 +1,43 @@
 import React, { useState, useEffect } from "react";
 
 function RealMoney() {
-    const [budget, setBudget] = useState("$0");
-    const [displayBudget, setDisplayBudget] = useState("$0");
+  const [budget, setBudget] = useState("$0");
+  const [displayBudget, setDisplayBudget] = useState("$0");
+  const [filename, setFilename] = useState("");
 
-    const handleBudgetChange = event =>{
-
-    if(event.target.value[0] == "$"){
-        // localStorage.setItem("budget", event.target.value);
-        // console.log("HELLLO", localStorage.getItem("budget"));
-        setBudget(event.target.value);
-    } 
-    else{
-        // localStorage.setItem("budget", "$" + event.target.value);
-        setBudget("$" + event.target.value)
+  const handleBudgetChange = (event) => {
+    if (event.target.value[0] == "$") {
+      // localStorage.setItem("budget", event.target.value);
+      // console.log("HELLLO", localStorage.getItem("budget"));
+      setBudget(event.target.value);
+    } else {
+      // localStorage.setItem("budget", "$" + event.target.value);
+      setBudget("$" + event.target.value);
     }
-    
+  };
 
-    };
-    
-    const changeBudget = () => {
-        setDisplayBudget(budget);
-        console.log("budget", budget);
-        localStorage.setItem("budget", budget);
-      };
+  const handleReceipt = (event) => {
+    console.log(event.target.files[0].path);
+    setFilename(event.target.value);
+    console.log("filename:", filename);
+  };
 
-      useEffect(() => {
-        if(localStorage.getItem("budget")){
-            setDisplayBudget(localStorage.getItem("budget"));
-        }
-        
-      },[]);
-    
+  const changeBudget = () => {
+    setDisplayBudget(budget);
+    console.log("budget", budget);
+    localStorage.setItem("budget", budget);
+  };
+
+  const processReceipt = () => {};
+
+  useEffect(() => {
+    if (localStorage.getItem("budget")) {
+      setDisplayBudget(localStorage.getItem("budget"));
+    }
+  }, []);
+
   return (
     <section class="min-h-screen bg-slate-100">
-      
       <div class="pl-16 grid grid-cols-2">
         <div class="h-full w-4/5 inline-block">
           <h2 class=" mb-2 text-4xl font-medium leading-tight text-primary mt-5 bg-amber-200   rounded-md w-3/5 p-2 ">
@@ -67,23 +70,26 @@ function RealMoney() {
               </tr>
             </tbody>
           </table>
-
-          <label
-            for="formFile"
-            class="mb-2 inline-block text-dark-green pt-12" 
-          >
+          <label for="formFile" class="mb-2 inline-block text-dark-green pt-12">
             Upload new receipt
           </label>
           <input
             class=" m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding py-[0.32rem] px-3 text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[margin-inline-end:0.75rem] file:[border-inline-end-width:1px] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-[0_0_0_1px] focus:shadow-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100"
             type="file"
             id="formFile"
+            onChange={handleReceipt}
           />
-
+          <button
+            type="button"
+            class="m-2 inline-block bg-celadon rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-gray-800 shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+            data-te-ripple-init
+            data-te-ripple-color="light"
+            onClick={processReceipt}
+          >
+            Process Receipt
+          </button>
           Total amount of submitted item:
         </div>
-
-        
 
         <div class="h-full w-4/5 inline-block">
           <h2 class="mt-0 mb-2 text-4xl font-medium leading-tight text-primary mt-5 bg-amber-200 rounded-md w-2/5 p-2 ">
@@ -96,7 +102,9 @@ function RealMoney() {
                 Current budget (per/month):
               </p>
               <div class="w-full text-2xl bg-slate-100 text-dark-green rounded-md  p-2  h-3/4 inline">
-                {localStorage.getItem("budget") ? localStorage.getItem("budget"): budget}
+                {localStorage.getItem("budget")
+                  ? localStorage.getItem("budget")
+                  : budget}
               </div>
               <p class="mb-4 text-3xl text-dark-green pt-3">
                 Enter new budget:
