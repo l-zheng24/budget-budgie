346bd625b0dd1b3534c07cb91069acf4e7ea6c8a
Taylor Liegel <tsliegel@gmail.com>
2023-04-02 02:23:53 -0400

    Expanded web scraper

--- backend/cbordScraper.py
+++ backend/cbordScraper.py
@@ -2,16 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import json
 
 # Taylor Liegel
 # 3/31/2023
-
-
-
-
-
-
 
 
 def obtain_info(username, password):
@@ -19,7 +15,8 @@ def obtain_info(username, password):
     options = Options()
     options.add_argument("headless")
     options.add_argument("--log-level=3")
-    driver = webdriver.Chrome(executable_path='/Users/lenazheng/Downloads/chromedriver_mac64/chromedriver', options=options)
+    driver = webdriver.Chrome(
+        executable_path='/Users/lenazheng/Downloads/chromedriver_mac64/chromedriver', options=options)
 
     driver.get("https://tribecard.wm.edu/login/cas.php")
 
@@ -37,7 +34,8 @@ def obtain_info(username, password):
 
     # Table time babey
 
-    tbody = driver.find_element(By.XPATH, '//*[@id="divContent"]/table[2]/tbody/tr/td[2]/div/table[1]/tbody')
+    tbody = driver.find_element(
+        By.XPATH, '//*[@id="divContent"]/table[2]/tbody/tr/td[2]/div/table[1]/tbody')
     valueDict = {}
 
     for row in tbody.find_elements(By.XPATH, './tr'):
@@ -48,12 +46,98 @@ def obtain_info(username, password):
 
         valueDict[name] = value
 
-
     # print(valueDict)
 
-    mealSwipes = driver.find_element(By.XPATH, '//*[@id="divContent"]/table[2]/tbody/tr/td[2]/div/table[2]/tbody/tr/td').text
+    mealSwipes = driver.find_element(
+        By.XPATH, '//*[@id="divContent"]/table[2]/tbody/tr/td[2]/div/table[2]/tbody/tr/td').text
 
     valueDict["Swipes"] = mealSwipes
 
-    json_object = json.dumps(valueDict, indent = 4)
+
+    individualDining = driver.find_element(
+        By.XPATH, '//*[@id="divContent"]/table[2]/tbody/tr/td[2]/div/table[1]/tbody/tr[2]/th/a')
+
+    individualDining.click()
+
+
+    monthSelection = driver.find_element(
+        By.XPATH, '//*[@id="svcHistForm"]/input[1]')
+
+    monthSelection.click()
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+        (By.XPATH, '//*[@id="divHist"]/table[1]/tbody/tr[1]/td[2]')))
+
+
+    init_val = driver.find_element(
+        By.XPATH, '//*[@id="divHist"]/table[1]/tbody/tr[1]/td[2]').text
+
+
+    valueDict["MonthStartDining"] = init_val.replace("$", "")
+
+    post_val = driver.find_element(
+        By.XPATH, '//*[@id="divHist"]/table[1]/tbody/tr[2]/td[2]').text
+
+    valueDict["MonthEndDining"] = post_val.replace("$", "")
+
+    driver.get("https://tribecard.wm.edu/student/welcome.php")
+
+    individualSwipes = driver.find_element(
+        By.XPATH, '//*[@id="divContent"]/table[2]/tbody/tr/td[2]/div/table[2]/tbody/tr/th/a')
+
+    individualSwipes.click()
+
+
+    monthSelection = driver.find_element(
+        By.XPATH, '//*[@id="svcHistForm"]/input[1]')
+
+    monthSelection.click()
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+        (By.XPATH, '//*[@id="divHist"]/table[1]/tbody/tr[1]/td[2]')))
+
+
+    init_val = driver.find_element(
+        By.XPATH, '//*[@id="divHist"]/table[1]/tbody/tr[1]/td[2]').text
+
+
+    valueDict["MonthStartSwipes"] = init_val.replace("$", "")
+
+    post_val = driver.find_element(
+        By.XPATH, '//*[@id="divHist"]/table[1]/tbody/tr[2]/td[2]').text
+
+    valueDict["MonthEndSwipes"] = post_val.replace("$", "")
+
+    # new
+
+    driver.get("https://tribecard.wm.edu/student/welcome.php")
+
+    individualSwipes = driver.find_element(
+        By.XPATH, '//*[@id="divContent"]/table[2]/tbody/tr/td[2]/div/table[1]/tbody/tr[1]/th/a')
+
+    individualSwipes.click()
+
+
+    monthSelection = driver.find_element(
+        By.XPATH, '//*[@id="svcHistForm"]/input[1]')
+
+    monthSelection.click()
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+        (By.XPATH, '//*[@id="divHist"]/table[1]/tbody/tr[1]/td[2]')))
+
+
+    init_val = driver.find_element(
+        By.XPATH, '//*[@id="divHist"]/table[1]/tbody/tr[1]/td[2]').text
+
+
+    valueDict["MonthStartExpress"] = init_val.replace("$", "")
+
+    post_val = driver.find_element(
+        By.XPATH, '//*[@id="divHist"]/table[1]/tbody/tr[2]/td[2]').text
+
+    valueDict["MonthEndExpress"] = post_val.replace("$", "")
+
+
+    json_object = json.dumps(valueDict, indent=4)
     return json_object
